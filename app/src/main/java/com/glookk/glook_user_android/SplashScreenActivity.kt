c181ad77d89519ece322964a0be3845e20b6c847
nedyudombat <nedyudombat@gmail.com>
2019-03-14 14:34:44 +0100

    implemented OnBoarding screen

--- app/src/main/java/com/glookk/glook_user_android/SplashScreenActivity.kt
+++ app/src/main/java/com/glookk/glook_user_android/SplashScreenActivity.kt
@@ -1,12 +1,44 @@
 package com.glookk.glook_user_android
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
+// A splash screen created by Glookk.com
 class SplashScreenActivity : AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
 
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, OnBoardingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate to OnBoarding Page with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
     }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
 }
+
+
