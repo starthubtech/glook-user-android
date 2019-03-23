c9855fa332e4cbc3f17607b9abe2a1cf61aae67d
nedyudombat <nedyudombat@gmail.com>
2019-03-23 18:18:32 +0100

    implemented user home screen

---
+++ app/src/main/java/com/glookk/glook_user_android/HomeSceenActivity.kt
@@ -0,0 +1,14 @@
+package com.glookk.glook_user_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeSceenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_sceen)
+
+
+    }
+}
