5bcc76aa11c0fc5865c3c2a7c11042247b616cd5
nedyudombat <nedyudombat@gmail.com>
2019-03-24 18:19:37 +0100

    implemented bookig page

---
+++ app/src/main/java/com/glookk/glook_user_android/BookingActivity.kt
@@ -0,0 +1,12 @@
+package com.glookk.glook_user_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BookingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking)
+    }
+}
